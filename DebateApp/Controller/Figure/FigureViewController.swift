0cde63b08ca089e240296f1230bf19b018793b15
MASAKI <al16046@shibaura-it.ac.jp>
2020-04-15 16:11:09 +0900

    円グラフと棒グラフ出力確認

--- DebateApp/Controller/Figure/FigureViewController.swift
+++ DebateApp/Controller/Figure/FigureViewController.swift
@@ -13,16 +13,25 @@ class FigureViewController: UIViewController {
 
     
     @IBOutlet weak var pieChartView: PieChartView!
+    @IBOutlet weak var barChartView: BarChartView!
+    
+    var flag = String()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        barChartView.backgroundColor = UIColor(red: 189/255, green: 195/255, blue: 199/255, alpha: 1)
+        pieChartView.backgroundColor = UIColor(red: 189/255, green: 195/255, blue: 199/255, alpha: 1)
 
         // Do any additional setup after loading the view.
-        setupGraph()
+        setPieGraph()
+        //setBarGragh()
+        self.view.bringSubviewToFront(pieChartView)
+        //pieChartView.removeFromSuperview()
     }
     
     //円グラフセット
-    func setupGraph() {
+    func setPieGraph() {
             pieChartView.usePercentValuesEnabled = true
             
             let values: [Double] = [0, 1, 1, 1, 1]
@@ -39,6 +48,34 @@ class FigureViewController: UIViewController {
             let chartData = PieChartData(dataSet: dataSet)
             
             pieChartView.data = chartData
+        flag = "pie"
+    }
+    
+    func setBarGragh(){
+        let rawData: [Int] = [20, 50, 70, 30, 60, 90, 40]
+        let entries = rawData.enumerated().map { BarChartDataEntry(x: Double($0.offset), y: Double($0.element)) }
+        let dataSet = BarChartDataSet(entries: entries)
+        let data = BarChartData(dataSet: dataSet)
+        barChartView.data = data
+        flag = "bar"
+        
+    }
+    
+    
+    @IBAction func switchChart(_ sender: Any) {
+        if flag == "pie"{
+            //self.view.addSubview(barChartView!)
+            //pieChartView.removeFromSuperview()
+            self.view.bringSubviewToFront(barChartView)
+            setBarGragh()
+        } else if flag == "bar"{
+            //self.view.addSubview(pieChartView)
+            //barChartView.removeFromSuperview()
+            self.view.bringSubviewToFront(pieChartView)
+            setPieGraph()
+
+        }
+        
     }
     
     
